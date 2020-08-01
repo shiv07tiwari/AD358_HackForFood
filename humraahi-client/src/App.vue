56fa4b1d2ce451857d697d87a5acccab2812aac6
Aayush Joglekar <aayushjog@gmail.com>
2020-08-01 10:28:31 +0530

    webapp: initalised web app

---
+++ humraahi-client/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <Navbar />
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ humraahi-client/src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navbar navbar-expand-lg app-navbar">
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNavAltMarkup"
+      aria-controls="navbarNavAltMarkup"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <router-link tag="a" :to="{name: 'hello'}" class="nav-item nav-link" active-class="active">Maintenance Portal</router-link>
+        <a class="nav-item nav-link" href="#">Road Library Portal</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "app-navbar",
+  props: {},
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.app-navbar {
+    background-color: #276EF1;
+    color: white;
+
+    .nav-link {
+        color: white;
+        text-decoration: none;
+    }
+
+}
+
+</style>
