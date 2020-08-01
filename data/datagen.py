188b3928c25a5dd3410ff3464d8f1be725628b12
trancenoid <shivansh.bhr@gmail.com>
2020-08-02 00:44:52 +0530

    data: dependency conflict fix in datagen-complaints

---
+++ data/datagen.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import random
+import numpy as np
+
+#complaint_id, road_id, defect_type, is_verified, is_resolved, assigned_insp_id, 
+# insp_rep_id, status, reported_month, reported_year, resolved_on_month, resolved_year
+
