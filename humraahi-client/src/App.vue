2dc749e7858430542da0091aa6981c14fb495951
Aayush Joglekar <aayushjog@gmail.com>
2020-08-02 08:55:05 +0530

    added road and complaint page

--- humraahi-client/src/App.vue
+++ humraahi-client/src/App.vue
@@ -3,24 +3,25 @@
     <div id="nav">
       <Navbar />
     </div>
-    <router-view/>
+    <transition name="fade" mode="out-in">
+      <router-view />
+    </transition>
   </div>
 </template>
 
 <script>
-
-import Navbar from '@/components/Navbar.vue';
+import Navbar from "@/components/Navbar.vue";
 
 export default {
   components: {
-    Navbar
-  }
-}
+    Navbar,
+  },
+};
 </script>
 
 <style lang="scss">
 #app {
-  font-family: 'Roboto', Helvetica, Arial, sans-serif;
+  font-family: "Roboto", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
@@ -30,5 +31,17 @@ export default {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.2s;
+  transition-property: opacity;
+  transition-timing-function: ease;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
 }
 </style>
--- humraahi-client/src/router/index.js
+++ humraahi-client/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import MaintenancePortal from '@/views/MaintenancePortal.vue'
 import RoadArchive from '@/views/RoadArchive.vue'
+import Road from '@/views/Road.vue'
+import Complaint from '@/views/Complaint.vue'
 
 Vue.use(VueRouter)
 
@@ -19,6 +21,16 @@ const routes = [
     path: '/roads',
     name: 'roads',
     component: RoadArchive
+  },
+  {
+    path: '/road/:id',
+    name: 'road',
+    component: Road
+  },
+  {
+    path: '/complaint/:id',
+    name: 'complaint',
+    component: Complaint
   },
 ]
 
