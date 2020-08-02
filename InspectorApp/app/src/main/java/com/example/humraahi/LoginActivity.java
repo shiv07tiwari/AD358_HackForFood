691cff86c7bc914d7f4bc1a2437ad7ef0dbbe445
Komal Bhadani <bhadanikomal27@gmail.com>
2020-08-02 11:54:28 +0530

    Add basic InspectorApp

---
+++ InspectorApp/app/src/main/java/com/example/humraahi/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.example.humraahi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
+        setContentView(R.layout.activity_login);
+        Button myButton = findViewById(R.id.login_button);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent i = new Intent(LoginActivity.this, InspectorHomeActivity.class);
+//                startActivity(i);
+
+            }
+        });
+    }
+}
