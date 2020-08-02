8f32b620852144f659b0e3b65670a4e884cd737d
Aayush Joglekar <aayushjog@gmail.com>
2020-08-03 01:41:33 +0530

    add road implemented

--- humraahi-client/src/views/AddRoad.vue
+++ humraahi-client/src/views/AddRoad.vue
@@ -8,7 +8,7 @@
           <div class="form-row">
             <div class="form-group col-md-3">
               <label>Road Code</label>
-              <input  type="text" class="form-control" v-model="details.id" />
+              <input type="text" class="form-control" v-model="details.road_id" />
             </div>
             <div class="form-group col-md-9">
               <label>Road Name</label>
@@ -57,6 +57,29 @@
               </select>
             </div>
           </div>
+          <div class="form-row">
+            <div class="form-group col-md-3">
+              <label>Active from (month)</label>
+              <input
+                v-model="details.activeMonth"
+                type="number"
+                min="1"
+                max="12"
+                class="form-control"
+              />
+            </div>
+            <div class="form-group col-md-3">
+              <label>Active from (year)</label>
+              <input
+                v-model="details.activeYear"
+                type="number"
+                min="1970"
+                max="2020"
+                class="form-control"
+                id="inputPassword4"
+              />
+            </div>
+          </div>
         </form>
       </div>
       <div class="app-form mt-4" style="width: 70%;">
@@ -98,12 +121,52 @@
           </div>
           <div class="form-row">
             <div class="form-group col-md-3">
-              <label>Active from (month)</label>
-              <input v-model="details.activeMonth" type="number" min="1" max="12" class="form-control" />
+              <label>IRI (m/km)</label>
+              <input v-model="details.iri" type="text" class="form-control" />
             </div>
             <div class="form-group col-md-3">
-              <label>Active from (year)</label>
-              <input v-model="details.activeYear" type="number" min="1970" max="2020" class="form-control" id="inputPassword4" />
+              <label>Single Axle Load (msa)</label>
+              <input v-model="details.single_axle_load" type="text" class="form-control" />
+            </div>
+            <div class="form-group col-md-3">
+              <label>Shoulder Width (ft)</label>
+              <input v-model="details.shoulder_width" type="text" class="form-control" />
+            </div>
+            <div class="form-group col-md-3">
+              <label>No Shoulder</label>
+              <input
+                v-model="details.no_shoulder"
+                type="number"
+                min="0"
+                max="1"
+                class="form-control"
+              />
+            </div>
+          </div>
+          <div class="form-row">
+            <div class="form-group col-md-3">
+              <label>Crack (m)</label>
+              <input v-model="details.crack" type="text" class="form-control" />
+            </div>
+            <div class="form-group col-md-3">
+              <label>Potholes</label>
+              <input v-model="details.potholes" type="number" min="0" class="form-control" />
+            </div>
+            <div class="form-group col-md-3">
+              <label>Rut</label>
+              <input v-model="details.rut" type="text" class="form-control" />
+            </div>
+            <div class="form-group col-md-3">
+              <label>Long Crack</label>
+              <input v-model="details.long_crack" type="text" class="form-control" />
+            </div>
+            <div class="form-group col-md-3">
+              <label>Bridge</label>
+              <input v-model="details.bridge" type="number" min="0" max="1" class="form-control" />
+            </div>
+            <div class="form-group col-md-3">
+              <label>Mountain</label>
+              <input v-model="details.mountain" type="number" min="0" max="1" class="form-control" />
             </div>
           </div>
         </form>
@@ -144,34 +207,102 @@ export default {
       districts: Object.entries(district),
       materials: Object.entries(materials),
       details: {
-          id: "",
-          name: "",
-          start: "",
-          end: "",
-          district: "",
-          category: "",
-          zone: "",
-          length: 0,
-          material: "",
-          depth: "",
-          lvd: "",
-          mvd: "",
-          hvd: "",
-          activeMonth: 1,
-          activeYear: 2020,
-          exitPoints: "",
-          remarks: ""
-      }
+        road_id: "",
+        name: "",
+        start: "",
+        end: "",
+        district: "",
+        category: "",
+        zone: "",
+        length: 0,
+        material: "",
+        depth: "",
+        lvd: "",
+        mvd: "",
+        hvd: "",
+        activeMonth: 1,
+        activeYear: 2020,
+        exitPoints: "",
+        remarks: "",
+        rut: "",
+        potholes: 0,
+        crack: "",
+        long_crack: "",
+        iri: "",
+        bridge: "",
+        mountain: "",
+        single_axle_load: "",
+        no_shoulder: "",
+        shoulder_width: "",
+      },
     };
   },
   methods: {
-
-      submitRoad() {
-          console.log(this.details);
-      }
-
+    submitRoad() {
+      console.log(this.details);
+    },
   },
-  mounted() {},
+  mounted() {
+    let roads = [];
+
+    for (let i = 0; i < 0; i++) {
+      let road = {
+        id: i + 1,
+        name: this.$faker().address.streetName() + " Road",
+        start: this.$faker().address.city(),
+        end: this.$faker().address.city(),
+        district: this.districts[
+          Math.floor(Math.random() * this.districts.length)
+        ][0],
+        category: this.categories[
+          Math.floor(Math.random() * this.categories.length)
+        ][0],
+        zone: this.zones[Math.floor(Math.random() * this.zones.length)][0],
+        length: Math.random() * 200,
+        material: this.materials[
+          Math.floor(Math.random() * this.materials.length)
+        ][0],
+        depth: Math.random() * 10,
+        lvd: Math.floor(Math.random() * 5000),
+        mvd: Math.random() * 100,
+        hvd: Math.floor(Math.random() * 2000),
+        activeMonth: Math.ceil(Math.random() * 12),
+        activeYear: 1980 + Math.floor(Math.random() * 40),
+        exitPoints: [
+          this.$faker().address.city(),
+          this.$faker().address.city(),
+          this.$faker().address.city(),
+          this.$faker().address.city(),
+        ],
+        remarks: this.$faker().lorem.sentence(),
+        rut: 2.2 + Math.random() * 21.8,
+        potholes: Math.floor(Math.random() * 20),
+        crack: 10 + Math.floor(Math.random() * 35),
+        long_crack: 2.5 + Math.random() * 26.5,
+        iri: 1.5 + Math.random() * 4.59,
+        bridge: Math.floor(Math.random() * 2),
+        transport: 1 + Math.ceil(Math.random() * 2),
+        monuments: 1 + Math.ceil(Math.random() * 5),
+        complaints: Math.ceil(Math.random() * 50),
+        priority: 0,
+        mountain: Math.floor(Math.random() * 2),
+        single_axle_load: 721418.14 + Math.random() * 900000,
+        no_shoulder: Math.floor(Math.random() * 2),
+        shoulder_width: 2.5 + Math.random() * 5.5,
+        average_temperature: 13 + Math.random() * 29,
+      };
+
+      road.priority = road.transport + road.monuments + road.complaints / 100;
+
+      delete road.transport;
+      delete road.monuments;
+      delete road.complaints;
+
+      roads.push(road);
+    }
+
+    // console.log(JSON.stringify(roads));
+  },
 };
 </script>
 
--- humraahi-client/src/views/enums.js
+++ humraahi-client/src/views/enums.js
@@ -1,11 +1,8 @@
 export const categories = {
     "ne": "National Expressway",
-    "sh": "State Highway",
     "nh": "National Highway",
     "rr": "Ring Road",
-    "ca": "City Category A",
-    "cb": "City Category B",
-    "cc": "City Category C"
+    "nr": "Normal Roads",
 };
 
 export const zones = {
