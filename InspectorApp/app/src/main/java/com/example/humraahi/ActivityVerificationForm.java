1d41a67f43f0f3a4adcf3fa0afe60283c026408d
Komal Bhadani <bhadanikomal27@gmail.com>
2020-08-02 19:59:13 +0530

    Update Verification Form

--- InspectorApp/app/src/main/java/com/example/humraahi/ActivityVerificationForm.java
+++ InspectorApp/app/src/main/java/com/example/humraahi/ActivityVerificationForm.java
@@ -6,7 +6,13 @@ import android.os.PersistableBundle;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
 import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Toast;
+
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
 public class ActivityVerificationForm extends AppCompatActivity {
     @Override
