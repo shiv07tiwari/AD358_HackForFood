46111d9ad6062cab9c2ae1df9b2dba8bff0c75fc
Aayush Joglekar <aayushjog@gmail.com>
2020-08-03 07:36:34 +0530

    fetching complaint by id

--- humraahi-client/src/views/Complaint.vue
+++ humraahi-client/src/views/Complaint.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-complaint">
-    <template v-if="!loaded">
+    <div v-show="!loaded">
       <div
         style="display: flex; width: 100%; justify-content: center; align-items: center; flex-direction: column; height: 70vh;"
       >
@@ -9,12 +9,12 @@
         </div>
         <h3 style="font-weight: 300;">Loading please wait.</h3>
       </div>
-    </template>
-    <template v-show="loaded">
+    </div>
+    <div v-show="loaded">
       <div class="header mt-4">
         <h1
           style="font-weight: 300; text-align:center;"
-        >Complaint ID: {{complaint.id ? complaint.id : dummy.id}}</h1>
+        >Complaint ID: {{complaint.complaint_id ? complaint.complaint_id : dummy.id}}</h1>
       </div>
       <div class="details mt-5" style="display: flex;">
         <div class="mapphotos" style="flex: 35% 0 0; margin-right: 2rem;">
@@ -25,7 +25,47 @@
           </div>
         </div>
         <div class="content" style="flex: 1">
-          <div id="accordion">
+          <button
+            type="button"
+            class="btn btn-primary"
+            data-toggle="modal"
+            data-target="#exampleModal"
+          >View Inspection Report</button>
+
+          <!-- Modal -->
+          <div
+            class="modal fade"
+            id="exampleModal"
+            tabindex="-1"
+            role="dialog"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+          >
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Inspection Report</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+                <div class="modal-body" style="padding: 4rem;">
+                  <div class="row">
+                    <div class="col col-md-4 mb-3" v-for="key in report_keys" :key="key">
+                      <h5 style="text-transform: capitalize;">{{key.split('_').join(' ')}}</h5>
+                      <p>{{ report[key] }}</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-success" data-dismiss="modal">Verify</button>
+                  <button type="button" class="btn btn-danger">Reject</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div id="accordion" class="mt-4">
             <div class="card">
               <div class="card-header" id="headingOne">
                 <h5 class="mb-0">
@@ -101,7 +141,7 @@
           </div>
         </div>
       </div>
-    </template>
+    </div>
   </div>
 </template>
 
@@ -114,6 +154,30 @@ export default {
       map: null,
       loaded: false,
       complaint: {},
+      report: {
+        sub_id: "1",
+        road_name: "MG Road, Indore",
+        inspection_codes: "Damage from cattle",
+        road_grades: "Grade-A",
+        radio_protocol_signs: "No",
+        description: "Dummy",
+        anticipated_turnout_constructed: "Dummy",
+        overhead_power_lines: "Dummy",
+        remedial_codes: "Dummy",
+        priority: "Dummy",
+        local_name: "Dummy",
+        kilometre_marker_boards: "Dummy",
+        hill: "Dummy",
+        stop_sign: "Dummy",
+        narrow_road_sections: "Dummy",
+        audit: "Dummy",
+        activities: "Dummy",
+        rail_crossing_present: "Dummy",
+        risk: "Dummy",
+        stream_culvert: "Hanging Outlet",
+        vehicle_usage: "Low",
+      },
+      report_keys: [],
       dummy: {
         id: this.$faker().random.uuid().substring(0, 5),
         location: this.$faker().address.city(),
@@ -121,24 +185,47 @@ export default {
     };
   },
   mounted() {
-    setTimeout(() => {
-      this.loaded = true;
+    this.axios
+      .post("complaint", {
+        op: "byid_kp",
+        args: parseInt(this.$route.params.id),
+      })
+      .then(({ data }) => {
+        console.log(data);
+        this.complaint = data[0];
+        
+        this.report_keys = Object.keys(this.report);
 
-      console.log(this.$refs);
+        console.log(this.$refs);
 
-      this.map = new window.google.maps.Map(this.$refs["map"], {
-        center: { lat: 23, lng: 72.5 },
-        zoom: 18,
-        width: 100,
+        this.map = new window.google.maps.Map(this.$refs["map"], {
+          center: { lat: this.complaint.lat, lng: this.complaint.lng },
+          zoom: 18,
+          width: 100,
+        });
+
+        new window.google.maps.Marker({
+          position: { lat: this.complaint.lat, lng: this.complaint.lng },
+          map: this.map,
+          title: "Issue of " + " " + this.complaint.defect_type,
+        });
+
+        this.loaded = true;
+      })
+      .catch((err) => {
+        console.error(err);
+        this.map = new window.google.maps.Map(this.$refs["map"], {
+          center: { lat: 23, lng: 72.5 },
+          zoom: 18,
+          width: 100,
+        });
+
+        new window.google.maps.Marker({
+          position: { lat: 23, lng: 72.5 },
+          map: this.map,
+          title: "Pothole",
+        });
       });
-
-       new window.google.maps.Marker({
-        position: { lat: 23, lng: 72.5 },
-        map: this.map,
-        title: "Pothole",
-      });
-
-    }, 300);
   },
 };
 </script>
@@ -150,6 +237,10 @@ export default {
 
 .app-complaint {
   padding: 2rem 5rem;
+
+  .modal-dialog {
+    min-width: 1200px;
+  }
 
   .details {
     display: flex;
